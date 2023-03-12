6ddcd9acc395262820c168f792a72cd2b91acab4
unknown <eng_nadia22@yahoo.com>
2023-03-12 21:07:15 +0200

    angular1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home' , component : HomeComponent},
+  {path : '' , component : HomeComponent},
+  {path : 'product' , component : ProductComponent},
+  {path : 'contact' ,component : ContactComponent },
+  {path : 'blog' , component : BlogComponent},
+  {path : 'about' , component : AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
